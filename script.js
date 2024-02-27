899f6b5eee32c1667c433f931a772c84e8e66f40
Rafael M V <rafaelmendoncavaz@hotmail.com>
2024-02-27 20:14:00 -0300

    Added two functions to work with testes.js

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
 function isPalindrome(str){
     /* Seu código aqui */
+    let backwards = ""
+    let forward = ""
+    const cleanStr = str.replace(/ /g, '').toLowerCase()
+    for(let i = 0; i < cleanStr.length; i++) {
+        forward += cleanStr[i]
+        backwards = cleanStr[i] + backwards
+    }
+    return forward === backwards
 }
 
 function arrayMaxMin(arr){
     /* Seu código aqui */
+    let maxNumber = 0
+    let minNumber = 0
+    let minMaxArray = []
+    for(let i = 0; i < arr.length; i++) {
+        if(arr[i] > maxNumber) {
+            maxNumber = arr[i]
+        }
+        if(i === 0 || arr[i] < minNumber) {
+            minNumber = arr[i]
+        }
+    }
+    minMaxArray.push(minNumber, maxNumber)
+    return minMaxArray
 }
